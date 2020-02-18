806958599d445f27bc210c7c63bc3a54413f0c41
Fredrik Nistor <quizz.0n@live.com>
2020-02-18 23:21:45 +0200

    First commit

---
+++ assets/js/custom.js
@@ -0,0 +1,27 @@
+$('#buy1').hover(function(){
+    $('#view1').css("display", "inline-block");
+});
+$('#buy2').hover(function(){
+    $('#view2').css("display", "inline-block");
+});
+$('#buy3').hover(function(){
+    $('#view3').css("display", "inline-block");
+});
+$('#buy4').hover(function(){
+    $('#view4').css("display", "inline-block");
+});
+$('#buy5').hover(function(){
+    $('#view5').css("display", "inline-block");
+});
+$('#buy6').hover(function(){
+    $('#view6').css("display", "inline-block");
+});
+$('#buy7').hover(function(){
+    $('#view7').css("display", "inline-block");
+});
+$('#buy8').hover(function(){
+    $('#view8').css("display", "inline-block");
+});
+$('#buy9').hover(function(){
+    $('#view9').css("display", "inline-block");
+});
